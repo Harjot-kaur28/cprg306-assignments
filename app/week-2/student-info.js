69c63f7710acd910a4bedf843eddec4cd8c2dd8b
Harjot-kaur28 <harjotkaurr2023@gmail.com>
2025-01-18 01:00:58 -0700

    changes in app\week-2\student-info.js file

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,9 +3,8 @@ import Link from "next/link";
 const StudentInfo = () => {
   return (
     <div>
-      <p>Name: Harjot Kaur</p>
+      <p>Harjot Kaur</p>
       <p>
-        GitHub Repository:{" "}
         <Link href="https://github.com/Harjot-kaur28/cprg306-assignments.git">
           https://github.com
         </Link>
