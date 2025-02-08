92749ccfd28fe5a383186d5fe5fc66c00cbde9fd
Harjot-kaur28 <harjotkaurr2023@gmail.com>
2025-02-08 15:21:08 -0700

    insert app\week-5\new-item.js

---
+++ app/week-5/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Week5Page() {
+  return (
+    <main className="bg-black flex flex-col items-center min-h-screen p-6">
+      
+      <NewItem />
+    </main>
+  );
+}
